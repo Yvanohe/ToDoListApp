d3b8c693ddac4de1b6459c5b0d428e6a9765f4dd
Yvanohe <yvanohe@gmail.com>
2023-11-17 14:40:11 +0100

    initial commit

---
+++ app/src/main/java/yvan/lubac/todolistapp/MainActivity.java
@@ -0,0 +1,27 @@
+package yvan.lubac.todolistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
+    }
+}
